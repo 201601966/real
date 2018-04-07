18b8068833045701d52f7c5a97bf4797abc10d34
munjeong <201601966@cs-cnu.org>
2018-04-07 15:48:01 +0900

    real___8

--- src/main/java/com/your/dream/real/App.java
+++ src/main/java/com/your/dream/real/App.java
@@ -8,13 +8,12 @@ public class App
 {
 	private App() 
 	{
-		throw new UnsupportedOperationException();
 		// do not create App class directly
 	}
 	
     public static void main( String[] args ) {
     	for(String arg : args) {
-    		System.err.println("parameter: " + arg);
+    		logger.log("parameter: " + arg);
     	}
     }
 }
